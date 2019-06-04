7834b25ec4c6a2a56ed22cc48e5caedfe183930f
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2019-06-04 14:19:24 +0900

    feat: localStorageを使用したカウントアップ機能を実装

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{ count }}</p>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class About extends Vue {
+  get count() {
+    return Number(localStorage.getItem('count'));
+  }
+
+  private created() {
+    const c = localStorage.getItem('count');
+    const count = c === null ? 0 : Number(c);
+    localStorage.setItem('count', String(count + 1));
+  }
+}
+</script>
